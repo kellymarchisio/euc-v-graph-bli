5ba0f9009da549116d8a9539ae01137dea53fc16
Justin Waltrip <justin.waltrip@protonmail.com>
2023-04-13 00:31:46 -0400

    working

--- dir_sgm.py
+++ dir_sgm.py
@@ -1,14 +1,73 @@
+import nltk
+import tqdm
+import itertools
+import tensorflow_datasets as tfds
+
+
+def process_dict_pairs(pair_file):
+    """Parses a dictionary pairs file.
+
+    Pairs as list of (srcwd, trgwd) tuples
+    L1 and L2 vocabularies as sets.
+    """
+    pairs = []
+    l1_words = set()
+    l2_words = set()
+    with open(pair_file) as f:
+        for line in f:
+            w1, w2 = line.split()
+            w1 = w1.strip()
+            w2 = w2.strip()
+            pairs.append((w1, w2))
+            l1_words.add(w1)
+            l2_words.add(w2)
+    return pairs, l1_words, l2_words
+
+
+def load_wiki_data(lang):
+    """
+    Load fastText wiki data for relevant languages
+    """
+    # TODO remove [:1%]
+    # load wiki40b data for language
+    ds = tfds.load(f'wiki40b/{lang}', split='train[:1%]')
+
+    # separate samples by special markers
+    special_markers = ["_START_ARTICLE_", "_START_SECTION_", "_START_PARAGRAPH_", "_NEWLINE_"]
+
+    # TODO add tqdm
+    wiki_data = []
+    for example in ds:
+        text = example["text"].numpy().decode("utf-8")
+        text = text.replace("\n", "")
+        for marker in special_markers:
+            text = text.replace(marker, "[SEP]")
+        docs = list(filter(lambda x: x != "", text.split("[SEP]")))
+        wiki_data.extend(docs)
+
+    return wiki_data
+
 
 def main():
     """
-    1. Load fastText wiki data for relevant languages
-    2. Load bilingual dictionaries for relvant language comparisons
-    3. For each word pair (w1, w2),
+    1. Load bilingual dictionaries for relvant language comparisons
+    2. Load fastText wiki data for each language
+    3. For each word pair (w1, w2) in each language:
         add directed edge e from w1 to w2 where w(e) = p(w2|w1) = p(w1, w2) / p(w1) (and vice versa)
     4. Run SGM on directed adjacency matrices
     5. Evaluate performance
     """
-    pass
+    src = "en"
+    trg = "de"
+
+    word_pairs, src_words, trg_words = process_dict_pairs(f"dicts/{src}-{trg}/train/{src}-{trg}.0-5000.txt.1to1")
+
+    for lang, words in [(src, src_words), (trg, trg_words)]:
+        wiki_data = load_wiki_data(lang)
+
+        for w1, w2 in itertools.combinations(words, 2):
+            # compute bigram count for w1 w2
+            pass
 
 
 if __name__ == "__main__":
